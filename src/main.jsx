60d0f4431e0840178261dcb8c2ed3f2b9af698e4
jookitchentv-cyber <juro3040@gmail.com>
2026-07-13 17:27:08 +0700

    Adjust watermark opacity, icon size, background-attachment fixed

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+document.addEventListener('contextmenu', (e) => {
+  if (e.target.tagName === 'IMG') e.preventDefault();
+});
+document.addEventListener('dragstart', (e) => {
+  if (e.target.tagName === 'IMG') e.preventDefault();
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
